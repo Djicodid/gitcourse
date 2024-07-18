635bee2368eb39139fdb5f27bbfa374c4967b45d
Piri <piriev.piri@mail.ru>
2024-07-18 20:23:32 +0300

    first commit

---
+++ CodeFile1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace ty
+{
+    class Point
+    {
+        public int x;
+        public void App(int x)
+        { Console.WriteLine(x); }
+
+        public int Sum(int x , int y)
+            { return x + y; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int Y = 90;
+int Z = Y + 90000;
+Console.WriteLine(Sum(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+int? Sum(int? x,int? y )
+{
+
+return x + y; 
+}
